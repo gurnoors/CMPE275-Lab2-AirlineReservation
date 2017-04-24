05222f31e56b6922c232d51ae2a44be2b1350774
gurnoors <gurnoor@gmail.com>
2017-04-24 10:45:31 -0700

    removed practice files

--- src/main/java/edu/sjsu/cmpe275/lab2/entities/Plane.java
+++
@@ -1,8 +0,0 @@
-package edu.sjsu.cmpe275.lab2.entities;
-
-public class Plane {
-    private int capacity;
-    private String model; 
-    private String manufacturer;
-    private int yearOfManufacture;
-}
